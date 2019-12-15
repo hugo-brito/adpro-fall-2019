5523ccfde71c04888e6065129e9fc046bb03fc3b
Hugo Brito <hubr@itu.dk>
2019-12-15 16:32:56 +0100

    sentiment and par

--- 070-sentiment/src/main/scala/Main.scala
+++ 070-sentiment/src/main/scala/Main.scala
@@ -15,7 +15,7 @@ import org.apache.spark.sql.types._
 
 object Main {
 
-	type Embedding = (String, List[Double])
+	type Embedding = (String, Vector)
 	type ParsedReview = (Integer, String, Double)
 
 	org.apache.log4j.Logger getLogger "org"  setLevel (org.apache.log4j.Level.WARN)
@@ -25,9 +25,9 @@ object Main {
 		.master  ("local[5]")
 		.getOrCreate
 
-	spark.conf.set("spark.executor.memory", "4g")
+	spark.conf.set("spark.executor.memory", "16g")
 
-  import spark.implicits._
+	import spark.implicits._
 
 	val reviewSchema = StructType(Array(
 			StructField ("reviewText", StringType, nullable=false),
@@ -44,77 +44,189 @@ object Main {
 			.rdd
 			.zipWithUniqueId
 			.map[(Integer,String,Double)] { case (row,id) =>
-          (id.toInt, s"${row getString 2} ${row getString 0}", row getDouble 1) }
+					(id.toInt, s"${row getString 2} ${row getString 0}", row getDouble 1) }
 			.toDS
 			.withColumnRenamed ("_1", "id" )
 			.withColumnRenamed ("_2", "text")
-			.withColumnRenamed ("_3", "overall")
+			.withColumnRenamed ("_3", "rating")
 			.as[ParsedReview]
 
-  // Load the GLoVe embeddings file
+	// Load the GLoVe embeddings file
 
-  def loadGlove (path: String): Dataset[Embedding] =
+	def loadGlove (path: String): Dataset[Embedding] =
 		spark
 			.read
 			.text (path)
-      .map  { _ getString 0 split " " }
-      .map  (r => (r.head, r.tail.toList.map (_.toDouble))) // yuck!
+			.map  { _ getString 0 split " " }
+			.map  (r => (r.head, Vectors.dense(r.tail.map (_.toDouble)))) // Nice!
 			.withColumnRenamed ("_1", "word" )
 			.withColumnRenamed ("_2", "vec")
 			.as[Embedding]
 
-  def main(args: Array[String]) = {
+	// Takes a datasets and splits the column with text into a Sequence of words instead.
 
-    val DATA_PATH = "/data/"
+	def cleanReviews (ds :Dataset[ParsedReview]) :Dataset[Row] = {
+		// To get our tokenizer completely right
+		// https://nlp.stanford.edu/software/tokenizer.html
+		new RegexTokenizer()
+			.setInputCol("text")
+			.setOutputCol("words")
+			.setPattern("[^a-zA-Z0-9_]+") //Ensures that we still keep words like "it's", but get rid of ".", "," etc.
+			.transform(ds)
+			.select("id", "words", "rating")
+	}
 
-    val glove  = loadGlove (s"${DATA_PATH}/glove.6B.50d.txt")
-    val reviews = loadReviews (s"${DATA_PATH}/reviews_small_1.json")
+	// Splits the text into seperate words and adds a count for each word.
 
-    // replace the following with the project code
-    glove.show
-    reviews.show
+	def splitText (ds :Dataset[Row]) :Dataset[Row] = ds
+		.flatMap(r => r.getSeq[String](1)
+		.map(i => (r.getInt(0), i, r.getDouble(2), 1.0))) //maps on the seq
+		.withColumnRenamed ("_1", "id" )
+		.withColumnRenamed ("_2", "words")
+		.withColumnRenamed ("_3", "rating")
+		.withColumnRenamed ("_4", "count")
 
-    // Train the sentiment perceptron here (this is *one* possible workflow, and it is slow ...)
-    //
-    //   - First clean the data
-    //      - Use the tokenizer to turn records with reviews into records with
-    //      lists of words
-		//         documentation: https://spark.apache.org/docs/latest/ml-features.html#tokenizer
-    //         output type: a collection of (Integer, Seq[String], Double)
-    //  - Second translate the reviews to embeddings
-    //      - Flatten the list to contain single words
-    //         output type: a collection (Integer, String, Double) but much
-    //         longer than input
-    //      - Join the glove vectors with the triples
-    //         output type: a collection (Integer, String, Double,
-    //         Array[Double])
-    //      - Drop the word column, we don't need it anymore
-    //         output type: a collection (Integer, Double, Array[Double])
-    //      - Add a column of 1s
-    //         output type: a collection (Integer, Double, Array[Double], Integer)
-    //      - Reduce By Key (using the first or two first columns as Key), summing the last column
-    //         output type: a collection (Integer, Double, Array[Double], Integer)
-    //         (just much shorter this time)
-    //      - In each row divide the Array (vector) by the count (the last column)
-    //         output type: a collection (Integer, Double, Array[Double])
-    //         This is the input for the classifier training
-    //  - Train the perceptron:
-    //      - translated the ratings from 1..5 to 1..3 (use map)
-    //      - make sure tha columns are named "id", "label", "features"
-    //      - follow the MultilayerPerceptronClassifier tutorial.
-    //      - Remember that the first layer needs to be #50 (for vectors of size
-    //      50), and the last needs to be #3.
-    //  - Validate the perceptron
-    //      - Either implement your own validation loop  or use
-	  //        org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-    //
-    // Any suggestions of improvement to the above guide are welcomed by
-    // teachers.
-    //
-    // This is an open programming exercise, you do not need to follow the above
-    // guide to complete it.
+	// Sums all vectors that are related to a given id.
+	// This is very similar to the solution in this link: 
+	// https://stackoverflow.com/questions/33899977/how-to-define-a-custom-aggregation-function-to-sum-a-column-of-vectors
+
+	def aggregateVectors (ds :Dataset[Row], vecSize :Int) :Dataset[Row] = ds
+		.select("id", "vec")
+		.rdd
+		.map{ case Row(id: Int, v: Vector) => (id, BDV(v.toDense.values)) }
+		.foldByKey(BDV.zeros[Double](vecSize))(_ += _)
+		.mapValues(v => Vectors.dense(v.toArray))
+		.toDF("idvec", "vec")
+
+	// Takes a rating of 5 and converts into a representation of 3.
+
+	def ratingMap (rating : Double) : Int = rating match {
+		case 3.0 => 1
+		case r if (r < 3.0) => 0
+		case _ => 2
+	}
+
+	// Takes a vector and divides every element with the given number.
+
+	def makeAverageVector (vec :Vector) (num :Double) :Vector = {
+		val arr = vec.toArray.map(_ / num)
+		Vectors.dense(arr)
+	}
+
+	// Takes two datasets and makes a new dataset that contains the correct
+	// format for training
+
+	def correctFormat (aggVec :Dataset[Row], wordCount :Dataset[Row]) :Dataset[Row] = {
+		wordCount
+			.join(aggVec, $"id" === $"idvec")
+			.map {r =>
+				val id = r.getAs[Int]("id"); val rat = r.getAs[Double]("rating")
+				val newrat = ratingMap(rat)
+				val vector = r.getAs[Vector]("vec"); val num = r.getAs[Double]("sum(count)")
+				val newVector = makeAverageVector(vector) (num)
+				(id, newrat, newVector)
+			}
+			.toDF("id", "label", "features")
+	}
+
+	def main(args: Array[String]) = {
+
+		val DATA_PATH = "data/"
+
+		// the data to be used for the k-fold and the perceptron
+		val REVIEW_DATASET = "test.json"
+
+		// the output will be saved as a csv file with the name of the input file used
+		val OUTPUT = s"${DATA_PATH}/output/${REVIEW_DATASET}"
+
+		val glove  = loadGlove (s"${DATA_PATH}glove.6B/glove.6B.50d.txt")
+		val reviews = loadReviews (s"${DATA_PATH}reviews/Musical_Instruments_5.json")
+
+		val cleanedReviews = cleanReviews(reviews)
+
+		// Joins together the gloves and cleaned reviews and outputs each word with the associated
+		// vector from the glove.
+		val reviewWithVectors = splitText(cleanedReviews).join(glove, $"words" === $"word")
+			.select("id", "rating", "word", "vec", "count")
+
+		val aggregatedVectors = aggregateVectors(reviewWithVectors, 50)
+
+		// Simply groups one the id and rating and uses an aggregate function to combine all the count columns.
+		val wordCount = reviewWithVectors
+			.groupBy("id", "rating")
+			.agg(sum("count"))
+
+		val dataSet = correctFormat (wordCount, aggregatedVectors)
+
+		val splits = dataSet.randomSplit(Array(0.9, 0.1), seed = 1234L)
+		val train = splits(0)
+		val test = splits(1)
+
+		val trainer = new MultilayerPerceptronClassifier()
+			.setMaxIter(50)
+			.setLayers(Array[Int](50, 5, 4, 3))
+			.setBlockSize(128)
+			.setSeed (1234L)
+
+		val paramGrid = new ParamGridBuilder().build()
+		val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+		val cv = new CrossValidator().setEstimator(trainer) // our MultiLayerPerceptronClassifier
+		.setEvaluator(evaluator)
+		.setEstimatorParamMaps(paramGrid).setParallelism(11).setNumFolds(10); // tried setParallelism(11) to see if there was any improvement in the time
+		val model = cv.fit(train);
+		val result = model.transform(test)  
+		result.map( r => (r.getInt(0), r.getInt(1), r.getAs[Vector](2).toString, r.getAs[Vector](3).toString, r.getAs[Vector](4).toString, r.getDouble(5))).withColumnRenamed ("_1", "id" )
+		.withColumnRenamed ("_2", "label")
+		.withColumnRenamed ("_3", "features")
+		.withColumnRenamed ("_4", "rawPrediction")
+		.withColumnRenamed ("_5", "probability")
+		.withColumnRenamed ("_6", "prediction")
+		.coalesce(1)
+		.write
+		.option("header","true")
+		.option("sep",",")
+		.mode("overwrite")
+		.csv("output/musical_instr.csv")
+		
 
 		spark.stop
-  }
-
+	}
 }
+
+/*
+	1. What data sets have you managed to run (including size)?
+
+		(5-core files)
+		Musical Instruments : size (10,261 reviews)
+		Toys and Games : size (167,597 reviews)
+
+	2. What accuracy have you obtained? With how many iterations? And with what layer configuration?
+	Show the variance of accuracy observed in cross validation.
+
+		Musical Instruments : 
+			accuracy: 86.64%
+			Layer config: 50, 5, 4, 3
+			Iteration: 1
+		Toys and Games : 
+			accuracy: 83.96%
+			Layer config: 50, 5, 4, 3
+			Iteration: 1
+
+		Due to the limitation of time we were only able to run through each data set once and not play
+		around with the layers to optimize our prediction, hence evaluating the accuracy did not make
+		sense on only 2 obsevations.
+
+	3. What degree of parallelization was obtained? (for instance contrast the wall clock time with
+	the CPU time). Note we are not asking you to optimize parallelization, just to report what you
+	obtained.
+
+		We played around with the .setParallelism(11), but were unable to report on wall time vs. CPU 
+		time. Hence we suspect we have achieved some level of parallelization, but unable to document
+		it, due to the time.
+
+	4. What extensions (if any) you have implemented? Have you tried another classifier? Another
+	network configuration? Running on a highly parallel cluster of machines, etc. ...
+
+		We have not tried any extension, as we were having issues simply getting it to run.
+
+*/
--- 090-par/src/main/scala/adpro/Par.scala
+++ 090-par/src/main/scala/adpro/Par.scala
@@ -25,132 +25,150 @@ import scala.language.implicitConversions
 
 object Par {
 
-  // an alias for this kind of function:
-  type Par[A] = ExecutorService => Future[A]
+	// an alias for this kind of function:
+	type Par[A] = ExecutorService => Future[A]
 
-  def run[A] (s: ExecutorService) (a: Par[A]) : Future[A] = a(s)
+	def run[A] (s: ExecutorService) (a: Par[A]) : Future[A] = a(s)
 
 
-  case class UnitFuture[A] (get: A) extends Future[A] {
-    def isDone = true
-    def get (timeout: Long, units: TimeUnit) = get
-    def isCancelled = false
-    def cancel (evenIfRunning: Boolean) : Boolean = false
-  }
+	case class UnitFuture[A] (get: A) extends Future[A] {
+		def isDone = true
+		def get (timeout: Long, units: TimeUnit) = get
+		def isCancelled = false
+		def cancel (evenIfRunning: Boolean) : Boolean = false
+	}
 
-  // wrap a value in a Par[A]
-  def unit[A] (a: A) :Par[A] = (es: ExecutorService) => UnitFuture(a)
+	// wrap a value in a Par[A]
+	def unit[A] (a: A) :Par[A] = (es: ExecutorService) => UnitFuture(a)
 
-  // map is shown in the book
-  def map2[A,B,C] (a: Par[A], b: Par[B]) (f: (A,B) => C) : Par[C] =
-    (es: ExecutorService) => {
-      val af = a (es)
-      val bf = b (es)
-      UnitFuture (f(af.get, bf.get))
-    }
+	// map is shown in the book
+	def map2[A,B,C] (a: Par[A], b: Par[B]) (f: (A,B) => C) : Par[C] =
+		(es: ExecutorService) => {
+			val af = a (es)
+			val bf = b (es)
+			UnitFuture (f(af.get, bf.get))
+		}
 
-  def fork[A] (a: => Par[A]) : Par[A] = es => es.submit(
-    new Callable[A] { def call = a(es).get }
-  )
+	def fork[A] (a: => Par[A]) : Par[A] = es => es.submit(
+		new Callable[A] { def call = a(es).get }
+	)
 
-  def lazyUnit[A] (a: =>A) : Par[A] = fork(unit(a))
+	def lazyUnit[A] (a: =>A) : Par[A] = fork(unit(a))
 
-  // Exercise 1 (CB7.4)
+	// Exercise 1 (CB7.4)
 
-  // Exercise 1. Use lazyUnit to write a function that converts any
-  // function A =>B to one that evaluates its result asynchronously
-  // (so it spawns a separate thread).
-  // def asyncF[A,B] (f: A => B) : A => Par[B]
+	// Exercise 1. Use lazyUnit to write a function that converts any
+	// function A =>B to one that evaluates its result asynchronously
+	// (so it spawns a separate thread).
+	// def asyncF[A,B] (f: A => B) : A => Par[B]
 
-  def asyncF[A,B] (f: A => B) : A => Par[B] = a => lazyUnit(f(a))
+	def asyncF[A,B] (f: A => B) : A => Par[B] = a => lazyUnit(f(a))
 
-  def map[A,B] (pa: Par[A]) (f: A => B) : Par[B] =
-    map2 (pa,unit (())) ((a,_) => f(a))
+	def map[A,B] (pa: Par[A]) (f: A => B) : Par[B] =
+		map2 (pa,unit (())) ((a,_) => f(a))
 
-  // Exercise 2 (CB7.5) Write a function sequence that takes a list
-  // of parallel computations (List[Par[B]]) and returns a parallel
-  // computation producing a list (Par[List[B]]). No additional primitives
-  // are required. Don’t call run, as we do not want this function
-  // to execute anything yet.
-    
-  def sequence[A] (ps: List[Par[A]]): Par[List[A]] = 
-  // the principle of using Sequence:
-  // List[Option[A]] -> Option[List[A]]
-  // List(Some(1),Some(2),Some(3),None)
-  // Some(List(1,2,3))
-  (es : ExecutorService) => {
-    ps match {
-      case Nil => UnitFuture(Nil:List[A])
-      case (head :: tail) => 
-        val a = ( head (es) ).get
-        val b = ( (sequence (tail)) (es) ).get
-      UnitFuture(a :: b)
-    }
-  }
+	// Exercise 2 (CB7.5) Write a function sequence that takes a list
+	// of parallel computations (List[Par[B]]) and returns a parallel
+	// computation producing a list (Par[List[B]]). No additional primitives
+	// are required. Don’t call run, as we do not want this function
+	// to execute anything yet.
 
-  // Exercise 3 (CB7.6) Implement parFilter, which filters elements
-  // of a list in parallel (so the predicate f is executed in parallel
-  // for the various lists elements).
+	def sequence[A] (ps: List[Par[A]]): Par[List[A]] =
+	// the principle of using Sequence:
+	// List[Option[A]] -> Option[List[A]]
+	// List(Some(1),Some(2),Some(3),None)
+	// Some(List(1,2,3))
+	(es : ExecutorService) => {
+		ps match {
+			case Nil => UnitFuture(Nil:List[A])
+			case (head :: tail) =>
+				val a = ( head (es) ).get
+				val b = ( (sequence (tail)) (es) ).get
+			UnitFuture(a :: b)
+		}
+	}
+	/**
+	 * Feeback:
+	 * I would have avoided using explicit recursion in sequence, even though it
+	 * is very clear what your code does, simply because it can be a little cognitively
+	 * difficult. For reference, here's my version:
+	 * def sequence[A] (ps: List[Par[A]]): Par[List[A]] =
+	 *  es => UnitFuture( ps.map (p => p(es).get) )
+	 */
 
-  // this is shown in the book:
-  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
-    val fbs: List[Par[B]] = ps.map(asyncF(f))
-    // asyncF: takes a function and applies that function on a value
-    // in here, we map async to the list ps, so that every element of such list will have
-    // asyncF waiting for the value.
-    sequence(fbs)
-    // we have a List[Par[B]], we do sequence in order to get List[Par[B]]
-  }
+	// Exercise 3 (CB7.6) Implement parFilter, which filters elements
+	// of a list in parallel (so the predicate f is executed in parallel
+	// for the various lists elements).
 
-  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
-     val para = parMap (as) (e => if (f(e)) Some(e) else None) // every filter is done in parallel
-     map (para) (l => l.flatten) // getting rid of None's is not.
-  }
+	// this is shown in the book:
+	def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+		val fbs: List[Par[B]] = ps.map(asyncF(f))
+		// asyncF: takes a function and applies that function on a value
+		// in here, we map async to the list ps, so that every element of such list will have
+		// asyncF waiting for the value.
+		sequence(fbs)
+		// we have a List[Par[B]], we do sequence in order to get List[Par[B]]
+	}
 
-  // Exercise 4: implement map3 using map2
-  //   def map2[A,B,C] (a: Par[A], b: Par[B]) (f: (A,B) => C) : Par[C]
+	def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+		 val para = parMap (as) (e => if (f(e)) Some(e) else None) // every filter is done in parallel
+		 map (para) (l => l.flatten) // getting rid of None's is not.
+	}
 
-  def map3[A,B,C,D] (pa: Par[A], pb: Par[B], pc: Par[C]) (f: (A,B,C) => D) : Par[D] = {
-    map2 (unit((pa, pb)), pc) ((a, c) => {
-      val (a1: A, b1: B) = a
-     f(a1, b1, c) })
-  }
+	// Exercise 4: implement map3 using map2
+	//   def map2[A,B,C] (a: Par[A], b: Par[B]) (f: (A,B) => C) : Par[C]
 
-  // shown in the book
+	def map3[A,B,C,D] (pa: Par[A], pb: Par[B], pc: Par[C]) (f: (A,B,C) => D) : Par[D] = {
+		map2 (unit((pa, pb)), pc) ((a, c) => {
+			val (a1: A, b1: B) = a
+		 f(a1, b1, c) })
+	}
+	/**
+	 * Very cool implementation of map3!
+	 */
 
-  def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean = p(e).get == p2(e).get
+	// shown in the book
 
-  // Exercise 5 (CB7.11)
+	def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean = p(e).get == p2(e).get
 
-  def choiceN[A] (n: Par[Int]) (choices: List[Par[A]]) :Par[A] = 
-    es => {
-      val intn = run(es) (n).get
-      run(es)(choices(intn))
-    }
-  def choice[A] (cond: Par[Boolean]) (t: Par[A], f: Par[A]) : Par[A] = es => {
-    val n = if(run(es) (cond).get) unit(0) else unit(1)
-    run(es)(choiceN (n) (List(t,f)))
-  }
-  // Exercise 6 (CB7.13)
+	// Exercise 5 (CB7.11)
 
-  def chooser[A,B] (pa: Par[A]) (choices: A => Par[B]): Par[B] =
-    es => {
-      val a = run(es) (pa).get
-      run(es)(choices(a))
-    }
+	def choiceN[A] (n: Par[Int]) (choices: List[Par[A]]) :Par[A] =
+		es => {
+			val intn = run(es) (n).get
+			run(es)(choices(intn))
+		}
+	def choice[A] (cond: Par[Boolean]) (t: Par[A], f: Par[A]) : Par[A] = es => {
+		val n = if(run(es) (cond).get) unit(0) else unit(1)
+		run(es)(choiceN (n) (List(t,f)))
+	}
+	// Exercise 6 (CB7.13)
 
-  def choiceNviaChooser[A] (n: Par[Int]) (choices: List[Par[A]]) :Par[A] = chooser(n)(choices)
-  def choiceViaChooser[A] (cond: Par[Boolean]) (t: Par[A], f: Par[A]) : Par[A] = es => run(es) (chooser(if(run(es) (cond).get) unit(0) else unit(1))(List(t,f)))
+	def chooser[A,B] (pa: Par[A]) (choices: A => Par[B]): Par[B] =
+		es => {
+			val a = run(es) (pa).get
+			run(es)(choices(a))
+		}
 
-  // Exercise 7 (CB7.14)
+	def choiceNviaChooser[A] (n: Par[Int]) (choices: List[Par[A]]) :Par[A] = chooser(n)(choices)
+	def choiceViaChooser[A] (cond: Par[Boolean]) (t: Par[A], f: Par[A]) : Par[A] = es =>
+		run(es) (chooser(if(run(es) (cond).get) unit(0) else unit(1))(List(t,f)))
+	/**
+	 * Feedback:
+	 * Your choiceViaChooser is quite verbose. Here's my version for comparison:
+	 * def choiceViaChooser[A] (cond: Par[Boolean]) (t: Par[A], f: Par[A]) : Par[A] =
+	 *  chooser (cond) (if (_) t else f)
+	 */
 
-  def join[A] (a : Par[Par[A]]) :Par[A] = es => {
-    val innerA = run(es) (a).get
-    run (es) (innerA)
-  }
-  class ParOps[A](p: Par[A]) {
+	// Exercise 7 (CB7.14)
 
-  }
+	def join[A] (a : Par[Par[A]]) :Par[A] = es => {
+		val innerA = run(es) (a).get
+		run (es) (innerA)
+	}
+	class ParOps[A](p: Par[A]) {
 
-  implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
+	}
+
+	implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
 }
